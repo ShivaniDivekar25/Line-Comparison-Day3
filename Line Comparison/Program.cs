454372d63d923e35040d86f044927ce9bf17082a
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-16 19:46:21 +0530

    Added Length Equality Check program

--- Line Comparison/Program.cs
+++ Line Comparison/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter 0 for Welcome Message program\nEnter 1 for Distance between line UC1");
+            Console.WriteLine("Enter 0 for Welcome Message program\nEnter 1 for Distance between line UC1\nEnter 2 for Line Equality check");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -22,6 +22,25 @@
                     var finalResult = DistanceBetweenLine.Distance(X1, X2, Y1, Y2);
 
                     Console.WriteLine("Distance between coordinates {0},{1} and {2},{3} is {4}", X1, Y1, X2, Y2, finalResult);
+                    break;
+                case 2:
+                    Console.WriteLine("Please enter X3 and Y3 coordinates");
+                    var X3 = Convert.ToDouble(Console.ReadLine());
+                    var Y3 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Please enter X4 and Y4 coordinates");
+                    var X4 = Convert.ToDouble(Console.ReadLine());
+                    var Y4 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Please enter X5 and Y5 coordinates");
+                    var X5 = Convert.ToDouble(Console.ReadLine());
+                    var Y5 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Please enter X6 and Y6 coordinates");
+                    var X6 = Convert.ToDouble(Console.ReadLine());
+                    var Y6 = Convert.ToDouble(Console.ReadLine());
+
+                    LineEqualityCheck.Equals(X3,Y3,X4,Y4,X5,Y5,X6,Y6);
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
